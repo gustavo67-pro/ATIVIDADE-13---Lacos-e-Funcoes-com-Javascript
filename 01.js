3d232959047074ff2c536e54fc152f6187cfb2d9
gustavo67-pro <gomescostagustavo89@gmail.com>
2026-06-18 13:46:43 -0300

    Create 01.js

---
+++ 01.js
@@ -0,0 +1,6 @@
+let numero = Number(prompt("insira o numero desejado: "))
+numerofixo = numero
+for (i=0;i<11;i++){
+    let resultado = numero * i
+    console.log(numero + " * " + i + " = " + resultado)
+}
