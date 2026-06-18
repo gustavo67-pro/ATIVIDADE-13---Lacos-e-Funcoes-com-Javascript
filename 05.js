4ad23065a0c3aa7aa9a670d7eca0b5d0495fb6de
gustavo67-pro <gomescostagustavo89@gmail.com>
2026-06-18 14:33:00 -0300

    Create 05.js

---
+++ 05.js
@@ -0,0 +1,6 @@
+let nota = Number(prompt("insira a nota do aluno: "))
+while((nota < 0 ) || (nota > 10)){
+    let nota = Number(prompt("inisra a nota do aluno: "))
+    alert("nota invalida")
+}
+alert("nota aceita")
