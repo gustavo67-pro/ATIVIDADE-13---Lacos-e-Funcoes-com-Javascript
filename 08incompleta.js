6f2ff54178253078bbf52dcc6b10facd29039ac2
diogoaugustonavais <diogonavais10@gmail.com>
2026-06-22 13:51:20 -0300

    Update 08incompleta.js

--- 08incompleta.js
+++ 08incompleta.js
@@ -1,9 +1,12 @@
-for (numero = 1; numero!==0; numero){
+let numero = Number(prompt("Digite um numero para ver qual é o maior e qual o menor digitado."))
+while (numero != 0){
      let numero = Number(prompt("Digite um numero para ver qual é o maior e qual o menor digitado."))
      maiormenor(numero);
 
 }
-console.log("O maior numero digitado foi "+maior+" e o menor digitado foi "+menor)
+maiorr = maiormenor(numero);
+menorr = maiormenor(numero);
+console.log("O maior numero digitado foi "+maiorr+" e o menor digitado foi "+menorr)
 
 function maiormenor(numero){
     maior = 0
@@ -13,6 +16,6 @@ function maiormenor(numero){
     } else if (numero<menor && numero !== 0 ){
         menor = numero
     }
-    return maior, menor
+    return maior
+    return menor
 }
-
